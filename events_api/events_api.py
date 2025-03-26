ff72475fa5e0dea8a0da3c1f3f8e796cde2b70a3
Maxson Almeida <maxsonferovante@gmail.com>
2025-03-26 09:11:43 -0300

    Remove verificação e salvamento de status no Sendinblue na classe EventsAPI

--- events_api/events_api.py
+++ events_api/events_api.py
@@ -48,36 +48,16 @@ class EventsAPI:
     
     
     def exist_sendinblue(self, name_certificate: str) -> bool:
-        # Verifica se o certificado já foi enviado para o Sendinblue        
-        if not os.path.exists("sendinblue_status.json"):
-            with open("sendinblue_status.json", "w") as file:
-                json.dump({}, file)
-        with open("sendinblue_status.json", "r") as file:
-            data = json.load(file)
-        return data.get(name_certificate, False)
+        pass
     
     def save_status_sendinblue(self, name_certificate: str) -> None:
-        # Salva o status de envio do certificado no Sendinblue, salvando os dados em um arquivo
-        if not os.path.exists("sendinblue_status.json"):
-            with open("sendinblue_status.json", "w") as file:
-                json.dump({}, file)
-        with open("sendinblue_status.json", "r") as file:
-            data = json.load(file)
-        data[name_certificate] = True
-        with open("sendinblue_status.json", "w") as file:
-            json.dump(data, file)
+        pass
 
     
 
         
-    def save_certificate(self, image_buffer: BytesIO, participant: Participant, name_certificate: str) -> None:    
-        if self.exist_sendinblue(name_certificate):
-                logger.info(f"Certificate already sent for {participant.first_name} {participant.last_name} ({participant.email})")
-                return
-        try:
-            if self.exist_sendinblue(name_certificate):
-                logger.info(f"Certificate already sent for {participant.first_name} {participant.last_name} ({participant.email})")
-                return
+    def save_certificate(self, image_buffer: BytesIO, participant: Participant, name_certificate: str) -> None:            
+        try:            
             url_save_certificate = "https://python.floripa.br/wp-json/event/v1/upload"
             data = {
                 "validation_code": participant.validation_code,
@@ -102,7 +82,6 @@ class EventsAPI:
                     logger.info(f"Response status code: {response.status_code}")
                     if response.status_code == 200:
                         logger.info(f"Certificate saved for {participant.first_name} {participant.last_name} ({participant.email})")
-                        self.save_status_sendinblue(name_certificate)
                         break
 
                     logger.error(f"Error saving certificate for {participant.first_name} {participant.last_name} ({participant.email}): {response.text}")

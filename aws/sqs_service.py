acbfc0e53721ea03459d5b702421698f4ade0390
Maxson Almeida <maxsonferovante@gmail.com>
2025-03-30 23:18:04 -0300

    Refactor SQS message handling in SQSService to use UUIDs for MessageGroupId and MessageDeduplicationId, ensuring unique message processing for FIFO queues.

--- aws/sqs_service.py
+++ aws/sqs_service.py
@@ -1,10 +1,10 @@
 from aws.boto_aws import get_instance_aws, ServiceNameAWS
 from config import config
+from botocore.exceptions import ClientError
 from typing import Dict
 import json
 import logging
-import boto3
-from botocore.exceptions import ClientError
+import uuid
 
 logger = logging.getLogger(__name__)
 
@@ -17,13 +17,13 @@ class SQSService:
         try:
             # Add MessageGroupId for FIFO queue
             # Using order_id as MessageGroupId to ensure messages for the same order are processed in order
-            message_group_id = str(message.get('order_id', 'default'))
-            
+            message_group_id = str(uuid.uuid4())
+            message_deduplication_id = str(uuid.uuid4())
             response = self.aws.send_message(
                 QueueUrl=self.queue_url,
                 MessageBody=json.dumps(message),
                 MessageGroupId=message_group_id,
-                MessageDeduplicationId=f"{message_group_id}_{message.get('email', '')}"  # Ensure unique messages per order/email
+                MessageDeduplicationId=message_deduplication_id,
             )
             logger.info(f"Mensagem enviada com sucesso: {response['MessageId']}")
             return response
